61cf49b22f06a750ea9c57c9e6965cf7ec390860
ShingoKobayashi <shingo37310010ve@gmail.com>
2022-10-24 01:13:58 +0900

    Add Fireabase Propaties

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ import 'package:shop_ui/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_ui/router.dart' as router;
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Widget checkScreen() {
     return WelcomePage();
   }
